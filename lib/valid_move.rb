41e8720294f4435c9ce6bf8b969d03a075e979f8
Christian Cordero <christiancordero092@gmail.com>
2021-05-07 19:09:19 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?(board, index)
   if (position_taken?(board, index))
     return false
-  elsif index < 0 || index > 
+  elsif index < 0 || index > 8
     return false
   else
     return true
